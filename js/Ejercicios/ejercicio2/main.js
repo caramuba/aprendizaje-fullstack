30d8f67a2e8d6193b1c0559b704a07b179cc8d00
CMunozBas <caramuba@gmail.com>
2025-08-29 00:11:39 -0300

    Nuevo ejersicio

---
+++ js/Ejercicios/ejercicio2/main.js
@@ -0,0 +1,19 @@
+
+// Clasificación de edades con arrow function
+/*
+const edad = prompt("Ingrese su edad:");
+const clasificacionEdades = () => {
+
+    if (edad <= 12) {
+        return "Niño";
+    } else if (edad <= 17) {
+        return "Adolescente";
+    } else if (edad <= 64) {
+        return "Adulto";
+    } else {
+        return "Adulto mayor";
+    }
+};
+console.log(clasificacionEdades());
+*/
+
