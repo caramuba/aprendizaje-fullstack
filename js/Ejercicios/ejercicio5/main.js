d56e0eb1ebae7b0cfbdb574ed642bfcbc5db3353
CMunozBas <caramuba@gmail.com>
2025-09-03 12:42:45 -0300

    commit prácatica de JS

--- js/Ejercicios/ejercicio5/main.js
+++ js/Ejercicios/ejercicio5/main.js
@@ -1,6 +1,6 @@
 function CalcularIva() {
     var precio = prompt("ingrese el precio al cual quiere calcular el IVA");
-    document.writeln("el precio sin IVA es : $" + precio + ". " + "El precio con IVA es : $" + (precio * 1.21) + ".")
+    document.writeln("El precio con IVA es : $" + (precio * 1.21) + ".")
 
 }
 CalcularIva();
