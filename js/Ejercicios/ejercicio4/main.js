baca06b28451a7bb5e07f301d026861801f1b37b
CMunozBas <caramuba@gmail.com>
2025-09-03 01:02:50 -0300

    usos y ámbitos de las variables

--- js/Ejercicios/ejercicio4/main.js
+++ js/Ejercicios/ejercicio4/main.js
@@ -1,5 +1,5 @@
 // Variables Globales y Locales
-
+/*
 let precio = parseFloat(prompt("Ingrese el precio del producto:"));
 let precioConIVA = 0; // Variable Global
 
@@ -11,3 +11,21 @@ function calcularIVA() {
 alert("Precio sin IVA: " + precio);
 calcularIVA();
 alert("Precio con IVA: " + precioConIVA);
+*/
+
+// usos y ambitos de las variables
+var variable1 = "Hola Tierra!"; // Variable Global
+
+if (variable1 != "") {
+    var variable2 = "Hola Marte!"; // asigna el valor a la variable global "variable2"
+}
+
+function mostrarVariables() {
+    var variable3 = "Hola Mercurio!"; // Variable Local
+    document.writeln("El contenido de la variable2 es: " + variable3 + "<br>");
+}
+
+document.writeln("El contenido de la variable1 es: " + variable1 + "<br>"); // Siempre se puede mostrar porque es global
+document.writeln("El contenido de la variable2 es: " + variable2 + "<br>");
+mostrarVariables(); // Llamo a la función para mostrar variable3
+// La variable3 no se puede mostrar aquí porque es local a la función mostrarVariables
